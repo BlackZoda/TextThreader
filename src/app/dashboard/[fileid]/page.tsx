c8ba6861f351ca3168369050c4fe973a1ef42652
Ivar Abusdal <ivar.abusdal@designerkontoret.no>
2024-01-09 18:51:02 +0100

    checkpoint before starting on the file uploader

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,4 +1,6 @@
 import { db } from "@/app/db"
+import ChatWrapper from "@/components/ChatWrapper"
+import PdfRenderer from "@/components/PdfRenderer"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { notFound, redirect } from "next/navigation"
 
@@ -28,8 +30,25 @@ const Page = async ({params} : PageProps) => {
     if (!file) notFound();
 
     return (
-        <div>{fileid}</div>
+        <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+            <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+                {/* left side*/}
+                <div className="flex-1 xl:flex">
+                    <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+                        <PdfRenderer />
+                    </div>
+                </div>
+
+                <div className="shrink-0 flex-[0.75] border-t border-gray-200
+                        lg:w-96 lg:border-l lg:brder-t-0">
+                    <ChatWrapper />
+                </div>
+            </div>
+        </div>
     )
 }
 
 export default Page;
+
+// view pdf including pdf options
+// chat to pdf including input field
